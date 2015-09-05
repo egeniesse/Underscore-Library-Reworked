69d192d3fc7a9bf49eed6ac006d07dba443f0e61
egeniesse <egeniesse@gmail.com>
2015-09-05 10:48:15 -0700

    So far, I've added a map function and a reduce function.

--- underscoreReworked.js
+++ underscoreReworked.js
@@ -1,9 +1,54 @@
 // UnderscoreReworked by Eric Geniesse
 
-function each (list, iterator){
-	for (var i = 0; i< list.length; i++){
-		iterator(list[i]);
-	}
-};
+function test(){
 
-console.log("Hello World")
+	function each (list, iterator){
+	
+		if (list.constructor === Object){
+			for (key in list){
+				iterator(list[key]);
+			};
+		}
+		else{
+			for (var i = 0; i< list.length; i++){
+				iterator(list[i]);
+			};
+		}
+	};
+
+
+	function map (list, iterator){
+
+		var newArr = [];
+
+		each(list, function(value){
+			newArr.push(iterator(value));
+		});
+
+		return newArr;
+
+	};
+
+	function reduce (list, iterator, start){
+
+		var total = start;
+
+		if (total.constructor === Array){
+			each(list, function(value){
+				iterator(total, value);
+			});
+		}
+
+		else{
+			each(list, function(value){
+				total = iterator(total, value);
+			});
+		}
+		return total;
+	};
+
+
+
+	console.log(reduce([1,2,3,4], function(num, mun){return num + mun}, 0))
+	console.log(reduce([1,2,3,4], function(array, num){array.push(num + 3);}, []))
+};
